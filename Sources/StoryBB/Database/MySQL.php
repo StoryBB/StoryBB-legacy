4a8e050c90e25fe8f9de0ecda07e74e16dbd2d9f
Arantor <sleeping@myperch.org>
2024-07-20 20:09:10 +0100

    Fix strictness on MySQL 8+

--- Sources/StoryBB/Database/MySQL.php
+++ Sources/StoryBB/Database/MySQL.php
@@ -129,7 +129,7 @@ class MySQL implements DatabaseAdapter
 			$this->select_db($this->db_name);
 		}
 
-		mysqli_query($this->connection, "SET SESSION sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'");
+		mysqli_query($this->connection, "SET SESSION sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'");
 
 		mysqli_set_charset($this->connection, 'utf8mb4');
 	}
